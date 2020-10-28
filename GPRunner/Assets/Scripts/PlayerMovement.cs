dc4f942a0a3811b0146ce2c1b69837fdf40d79c9
dennAbreu <34317914+DennisPlusPlus@users.noreply.github.com>
2020-10-27 20:43:35 -0400

    Updated: PlayerMovement Script, Platform GameObject, Player GameObject
    -Updated Player Script to allow smoother movement.
    -Platform and Player both have colliders.
    -Player has gravity

--- GPRunner/Assets/Scripts/PlayerMovement.cs
+++ GPRunner/Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     bool dead = false; // Track if our player is dead.
+    bool isGrounded = true; //Checks to see if player is grounded
+    public Vector3 jump;
+    public float moveSpeed;
+    public float stopSpeed;
+    public float jumpForce;
+
+    private float delayTime;
 
     Rigidbody rb;
 
@@ -15,12 +23,15 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        this.rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0, 1.0f, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (dead)
         {
             return;
@@ -28,23 +39,58 @@ public class PlayerMovement : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.LeftArrow))) // If presses A or <-
         {
-            // Changes the velocity component on the player to move its x position to the left side.
-            rb.velocity = new Vector3(-2, this.transform.position.y, this.transform.position.z);
+            
+                // Changes the velocity component on the player to move its x position to the left side.
+                //rb.velocity = new Vector3(-2, this.transform.position.y*moveSpeed, this.transform.position.z);
+                rb.velocity = new Vector3(-moveSpeed, 0, this.transform.localPosition.z);
+                StartCoroutine(stopMove());
+            
+
         }
         else if (Input.GetKeyDown(KeyCode.D) || (Input.GetKeyDown(KeyCode.RightArrow))) // D or ->
         {
             // Changes the velocity component on the player to move its x position to the left side.
-            rb.velocity = new Vector3(2, this.transform.position.y, this.transform.position.z);
+            rb.velocity = new Vector3(moveSpeed, 0, this.transform.localPosition.z);
+            StartCoroutine(stopMove());
+
         }
         else if (Input.GetKeyDown(KeyCode.S) || (Input.GetKeyDown(KeyCode.DownArrow))) // S or down arrow (duck)
         {
             // Changes the velocity component on the player to move its y position down.
-            rb.velocity = new Vector3(this.transform.position.y, -0.5f, this.transform.position.z);
+            rb.velocity = new Vector3(this.transform.localPosition.y, -0.5f, this.transform.localPosition.z);
+            StartCoroutine(stopMove());
+
         }
-        else if (Input.GetKeyDown(KeyCode.Space)) // if presses space (jump)
+        else if (Input.GetKeyDown(KeyCode.Space) && isGrounded) // if presses space (jump) and player is not already in air
         {
+
             // Changes the velocity component on the player to move its y position up.
-            rb.velocity = new Vector3(this.transform.position.y, 2, this.transform.position.z);
+            //rb.velocity = new Vector3(this.transform.position.y, 2, this.transform.position.z);
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            StartCoroutine(stopMove());
+            isGrounded = false;
+        }
+
+        isGrounded = true;
+
+    } //end Update
+
+    //Stops movement after a certain amount of time. "stopSpeed" is the variable 
+    //change "stopSpeed" depending on how fast you want the character to stop moving after a key press
+    //Note: We can play around with moveSpeed + stopSpeed in order to creater cleaner and quicker movements or make it "lazier" depending on what we want
+    //Good default numbers: moveSpeed = 8, stopSpeed = 0.2, jumpForce = 20 
+
+    IEnumerator stopMove()
+    {
+        yield return new WaitForSeconds(stopSpeed);
+        rb.velocity = new Vector3(0, 0, 0);
+    }
+
+    private void onFloorColCheck(Collision collision)
+    {
+        if(collision.gameObject.name == "Platform")
+        {
+            isGrounded = true;
         }
     }
 
