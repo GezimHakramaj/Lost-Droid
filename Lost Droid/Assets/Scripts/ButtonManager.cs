e7d2ee4f5aa54cd2b67ca090b1706c80f240b98c
Gezim Hakramaj <gezimhakramaj@gmail.com>
2020-10-31 01:31:24 -0400

    Updating main branch

---
+++ Lost Droid/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void SwitchScene()
+    {
+        switch (this.name)
+        {
+            case ("Play Button"):
+                SceneManager.LoadScene("Gameplay");
+                break;
+            case ("Quit Button"):
+                SceneManager.LoadScene("Main Menu");
+                break;
+            case ("Restart Button"):
+                SceneManager.LoadScene("Gameplay");
+                this.gameObject.SetActive(false);
+                break;
+                /* case ("Quit MM Button"):
+                    QUIT GAME.
+                    */
+        }
+    }
+}
---
+++ Lost Droid/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    public TextMeshProUGUI timeText;
+    public TextMeshProUGUI gameOverTimeText;
+    public TextMeshProUGUI gameOverBestTimeText;
+    public GameObject gameOverMenu;
+
+    int currentTime;
+    int bestTime;
+    float timer = 0.0f;
+
+    private void Awake()
+    {
+        Kill_Player.killPlayer += TimeStop;
+    }
+
+    void Start()
+    {
+        currentTime = 0;
+        UpdateUI();
+        bestTime = PlayerPrefs.GetInt("BestTime", 0);
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        currentTime = (int)(timer % 60f);
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        timeText.text = currentTime + "";
+    }
+
+    void TimeStop()
+    {
+        enabled = false;
+        gameOverMenu.SetActive(true);
+        gameOverTimeText.text = timeText.text;
+        if(currentTime > bestTime)
+        {
+            PlayerPrefs.SetInt("BestTime", currentTime);
+        }
+        gameOverBestTimeText.text = PlayerPrefs.GetInt("BestTime", 0) + "";
+    }
+
+    private void OnDestroy()
+    {
+        Kill_Player.killPlayer -= TimeStop;
+    }
+}
