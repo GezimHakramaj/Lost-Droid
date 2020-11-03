97346efb0860b34bab6361f5a558161d10f3ac9c
Gezim Hakramaj <gezimhakramaj@gmail.com>
2020-11-03 01:09:13 -0500

    Updated game mechanics
    Updated player to 'float' rather than walk through space. Added rotation to obstacles to simulate debris. Added new prefab obstacles. Updated player scripts as well as obstacle scripts.

--- Lost Droid/Assets/Scripts/Kill_Player.cs
+++ Lost Droid/Assets/Scripts/Kill_Player.cs
@@ -6,7 +6,6 @@ public class Kill_Player : MonoBehaviour
 {
     public delegate void KillHandler();
     public static event KillHandler killPlayer;
-    public bool val = true;
 
     private void OnTriggerEnter(Collider collider)
     {
--- Lost Droid/Assets/Scripts/PlayerMovement.cs
+++ Lost Droid/Assets/Scripts/PlayerMovement.cs
@@ -1,116 +1,59 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    bool dead = false; // Track if our player is dead.
-    bool isGrounded = true; //Checks to see if player is grounded
-    public Vector3 jump; 
-    public float moveSpeed; //Increase this to increase horizontal distance covered for each keypress
-    public float stopSpeed; //Stops all motion after a keypress after delay set to value of stopSpeed;
-    public float jumpForce; //used to multiply with Y vector to determine how high player jumps
-    public float fallMultiplier = 3.5f; //Value taken from Youtube Tutorial.
-    private float delayTime;
-
-    private float distToGround;
-
+    public float enginePower;
+    bool dead;
     Rigidbody rb;
-    CapsuleCollider cc;
 
-
-
-    private void Awake() // Subcribing to killPlayer event.
+    private void Awake()
     {
         Kill_Player.killPlayer += OnDeath;
-
-        this.rb = GetComponent<Rigidbody>();
-        this.cc = GetComponent<CapsuleCollider>();
+        
     }
 
     private void Start()
     {
-   
-        jump = new Vector3(0, 1.0f, 0);
-        distToGround = cc.bounds.extents.y;
-
+        dead = false;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (dead)
-        {
             return;
-        }
-
-        if ((Input.GetKeyDown(KeyCode.A) || (Input.GetKeyDown(KeyCode.LeftArrow))) && getGroundedVal()) // If presses A or <-
+        else
         {
-            
-                // Changes the velocity component on the player to move its x position to the left side.
-                //rb.velocity = new Vector3(-2, this.transform.position.y*moveSpeed, this.transform.position.z);
-                rb.velocity = new Vector3(-moveSpeed, 0, this.transform.localPosition.z);
-                StartCoroutine(stopMove());
-          
-
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            {
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                    rb.velocity = new Vector3(rb.velocity.x - enginePower, rb.velocity.y - enginePower, rb.velocity.z);
+                else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                    rb.velocity = new Vector3(rb.velocity.x + enginePower, rb.velocity.y - enginePower, rb.velocity.z);
+                else
+                    rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - enginePower, rb.velocity.z);
+            }
+            else if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                    rb.velocity = new Vector3(rb.velocity.x - enginePower, rb.velocity.y + enginePower, rb.velocity.z);
+                else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                    rb.velocity = new Vector3(rb.velocity.x + enginePower, rb.velocity.y + enginePower, rb.velocity.z);
+                else
+                    rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + enginePower, rb.velocity.z);
+            }
+            else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                rb.velocity = new Vector3(rb.velocity.x - enginePower, rb.velocity.y, rb.velocity.z);
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                rb.velocity = new Vector3(rb.velocity.x + enginePower, rb.velocity.y, rb.velocity.z);
+            }
         }
-        else if ((Input.GetKeyDown(KeyCode.D) || (Input.GetKeyDown(KeyCode.RightArrow))) && getGroundedVal()) // D or ->
-        {
-            // Changes the velocity component on the player to move its x position to the left side.
-            rb.velocity = new Vector3(moveSpeed, 0, this.transform.localPosition.z);
-            StartCoroutine(stopMove());
-
-        }
-        else if ((Input.GetKeyDown(KeyCode.S) || (Input.GetKeyDown(KeyCode.DownArrow))) && getGroundedVal()) // S or down arrow (duck)
-        {
-            // Changes the velocity component on the player to move its y position down.
-            rb.velocity = new Vector3(this.transform.localPosition.y, -0.5f, this.transform.localPosition.z);
-            StartCoroutine(stopMove());
-
-        }
-        else if (Input.GetKeyDown(KeyCode.Space) && getGroundedVal()) // if presses space (jump) and player is not already in air
-        {
-
-            // Changes the velocity component on the player to move its y position up.
-            //rb.velocity = new Vector3(this.transform.position.y, 2, this.transform.position.z);
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            StartCoroutine(stopMove());
-            isGrounded = false;
-        }
-
-        //isGrounded = true;
-
-        //Note: Default Gravity values can be set/altered within Unity. Edit > Project Settings... > Physics > Gravity
-        //Gravity is enabled in Player inspector window
-
-        if (rb.velocity.y < 0)
-        {
-            rb.velocity += Vector3.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        }
-
-        
-
-    } //end Update
-
-    //Stops movement after a certain amount of time. "stopSpeed" is the variable 
-    //change "stopSpeed" depending on how fast you want the character to stop moving after a key press
-    //Note: We can play around with moveSpeed + stopSpeed in order to creater cleaner and quicker movements or make it "lazier" depending on what we want
-    //Good default numbers: moveSpeed = 8, stopSpeed = 0.2, jumpForce = 20 
-
-
-    public bool getGroundedVal()
-    {
-        return Physics.Raycast(transform.position, -Vector3.up, distToGround +0.1f);
     }
-
-    
-    IEnumerator stopMove()
-    {
-        yield return new WaitForSeconds(stopSpeed);
-        rb.velocity = new Vector3(0, 0, 0);
-    }
-
-
 
     private void OnDeath()
     {
@@ -120,7 +63,5 @@ public class PlayerMovement : MonoBehaviour
     private void OnDestroy()
     {
         Kill_Player.killPlayer -= OnDeath;
-        
     }
-
 }
