1af5204a180a8888fa875b8b6ab1d507fe59bc68
JohnnnnJ <JohnnnnJ@users.noreply.github.com>
2020-11-05 21:03:54 -0500

    update

---
+++ Lost Droid/Assets/Scripts/Load.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float timer = 0;
+    int ctime = 0; 
+
+    void Update()
+    { 
+        timer += Time.deltaTime;
+        ctime = (int)(timer % 60f);
+        if (ctime > 1)
+            SceneManager.LoadScene("Gameplay");
+    }
+
+    
+}
