b338fcc0a72d0eec86b8a80e59d44dcaa414a081
JohnnnnJ <JohnnnnJ@users.noreply.github.com>
2020-11-05 20:34:27 -0500

    update

---
+++ Lost Droid/Assets/Scripts/Fall_Death.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fall_Death : MonoBehaviour
+{
+    public delegate void FallHandler();
+    public static event FallHandler playerFall;
+    public bool val = true;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+            playerFall();
+
+    }
+
+}
---
+++ Lost Droid/Assets/Scripts/SoundManagement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManagement : MonoBehaviour
+{
+
+    public static AudioClip moveSound, jumpSound, crashSound, fallSound;
+    static AudioSource audioSrc = new AudioSource();
+
+    // Use this for initialization
+    void Start()
+    {
+        moveSound = Resources.Load<AudioClip>("move_sfx");
+        jumpSound = Resources.Load<AudioClip>("jump_sfx");
+        crashSound = Resources.Load<AudioClip>("crash_sfx");
+        fallSound = Resources.Load<AudioClip>("fall_sfx");
+        
+
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip)
+        {
+            case "moveSound":
+                audioSrc.PlayOneShot(moveSound);
+                break;
+            case "jumpSound":
+                audioSrc.PlayOneShot(jumpSound);
+                break;
+            case "crashSound":
+                audioSrc.PlayOneShot(crashSound);
+                break;
+            case "fallSound":
+                audioSrc.PlayOneShot(fallSound);
+                break;
+
+
+        }
+    }
+}
